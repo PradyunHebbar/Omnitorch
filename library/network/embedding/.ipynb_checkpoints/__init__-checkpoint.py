4ff1d4d4d8c95d7a3b517c6671cd994d779a9b63
Pradyun Hebbar <phebbar@login25.chn.perlmutter.nersc.gov>
2024-11-13 05:21:36 -0800

    Removed checkpoints

--- library/network/embedding/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,4 +0,0 @@
-from .Local_embedding import LocalEmbeddingLayer
-from .Fourier_projection import FourierEmbedding
-
-__all__ = ['LocalEmbeddingLayer', 'FourierEmbedding']
--- library/network/transformers/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from .petbody_transformer import TransformerBlockModule
-from .petclassifier_transformer import ClassifierTransformerBlockModule
-from .petgenerator_transformer import GeneratorTransformerBlockModule
-
-__all__ = [
-    'BodyTransformerBlock',
-    'ClassifierTransformerBlockModule',
-    'GeneratorTransformerBlockModule'
-]
